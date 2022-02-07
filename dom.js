bf80a057dd336df2e3a78d1e949acdbb972c279f
Adam Brown <abrown0596@hotmail.co.uk>
2022-02-07 17:23:08 +0000

    End of day 1 garages - AB

---
+++ dom.js
@@ -0,0 +1,6 @@
+export const inputUpdateGarageName = document.getElementById(`inputUpdateGarageName`);
+export const inputUpdateGarageID = document.getElementById(`inputUpdateGarageID`);
+export const buttonUpdateGarage = document.getElementById(`buttonUpdateGarage`);
+
+export const inputDeleteGarageID = document.getElementById(`inputDeleteGarageID`);
+export const buttonDeleteGarage = document.getElementById(`buttonDeleteGarage`);
