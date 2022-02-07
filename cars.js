aa3b518b198289e0cddc86e343f2b7e4653ced7a
Ayshwaria Patel <ashi_patel@icloud.com>
2022-02-07 17:31:02 +0000

    Update/Delete

--- cars.js
+++ cars.js
@@ -5,25 +5,11 @@ import * as DOM from './dom.js';
 DOM.UpdateCar.onclick = () => PutCar();
 DOM.DeleteCar.onclick = () => DeleteCar();
 
-// delete (delete)
-app.delete('/delete/:id', (req,res) => {
-    console.log(`\nUpdate - PUT`);
-    //deleting item in our DB
-    db.remove({ _id: req.params.id }, (err, item) => {
-      // if there is an error, send back the error
-      if (err) res.send(err);
-      // otherwise 200 - Deleted the item (JSON)
-      res.status(200).send(`${JSON.stringify(item)}`);
-      // log it to console
-      console.log(`Deleted item: ${JSON.stringify(item)}`)
-    });
-  });
-
 //Delete (DELETE)
-const PutCar = () => {
+const DeleteCar = () => {
     DOM.CarOutput.innerHTML = ``;
 
-    axios.delete(`http://localhost:8080/car/delete/${DOM.CarName.value}`)
+    axios.delete(`http://localhost:8080/car/delete/${DOM.CarId.value}`)
         .then((response) => {
             console.log(response);
             DOM.CarOutput.innerHTML = `${JSON.stringify(response.data)}`;
@@ -32,3 +18,15 @@ const PutCar = () => {
           });
 }
 
+//Update (PUT)
+const PutCar = () => {
+    DOM.CarOutput.innerHTML = ``;
+
+    axios.put(`http://localhost:8080/car/update/${DOM.CarId.value}`)
+        .then((response) => {
+            console.log(response);
+            DOM.CarOutput.innerHTML = `${JSON.stringify(response.data)}`;
+        }).catch((err) => {
+            console.log(err);
+        });
+}
--- dom.js
+++ dom.js
@@ -1,8 +1,16 @@
-//Update (PUT) car
-export const CarName = document.getElementById(`CarName`);
-export const UpdateCar = document.getElementById(`CarUpdate`);
+//Car Name and Output
+export const CarId = document.getElementById(`CarID`);
 export const CarOutput = document.getElementById(`CarOutput`);
 
+//Update (PUT) car
+export const UpdateCar = document.getElementById(`CarUpdate`);
+
+export const CarModelUpdate = document.getElementById(`ModelUpdate`);
+export const CarNameUpdate = document.getElementById(`NameUpdate`);
+export const CarColourUpdate = document.getElementById(`ColourUpdate`);
+export const CarMakeUpdate = document.getElementById(`MakeUpdate`);
+export const CarDoorUpdate = document.getElementById(`DoorsUpdate`);
+export const CarGarageUpdate = document.getElementById(`GarageUpdate`);
+
 //Delete (DELETE) car
-export const CarName = document.getElementById(`carName`);
 export const DeleteCar = document.getElementById(`CarDelete`);
