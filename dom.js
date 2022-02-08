6f6d7dde393a61cc83f1c6d9a4bbec6a8164eebe
Jesse <kamauj93@gmail.com>
2022-02-08 11:55:00 +0000

    Added all functionalities to button!

--- dom.js
+++ dom.js
@@ -1,14 +1,48 @@
+//GARAGE
+//Read Garage(READ)
+export const readGarageButton = document.getElementById(`readGarageButton`);
+//READ Garage ID (READ)
+export const getGarageID = document.getElementById(`getGarageID`);
+export const specificGarageButton = document.getElementById(`specificGarageButton`);
+//Create Garage(POST)
+export const garageName = document.getElementById(`garageName`);
+export const createGarageButton = document.getElementById(`createGarageButton`);
+export const garageOutput = document.getElementById(`garageOutput`);
+//Delete Garage (DELETE)
+export const delGarageID = document.getElementById(`delGarageID`);
+export const deleteGarageButton = document.getElementById(`deleteGarageButton`);
+//Update Garage (PUT)
+export const updateGarageID = document.getElementById(`updateGarageID`);
+export const updateName = document.getElementById(`updateName`);
+export const updateGarageButton = document.getElementById(`updateGarageButton`);
 
 
-export const CarModelCreate = document.getElementById(`ModelCreate`);
-export const CarNameCreate = document.getElementById(`NameCreate`);
-export const CarColourCreate = document.getElementById(`ColourCreate`);
-export const CarMakeCreate = document.getElementById(`MakeCreate`);
-export const CarDoorCreate = document.getElementById(`DoorsCreate`);
-
-export const CarPost = document.getElementById(`CarCreate`);
-
-export const CarID = document.getElementById(`CarID`);
-
-export const CarSearch = document.getElementById('CarSearch');
-export const SearchID = document.getElementById(`SearchID`);
+//CARS
+//Read Cars(READ)
+export const readCarButton = document.getElementById(`readCarButton`);
+//READ Car ID (READ)
+export const getCarID = document.getElementById(`getCarID`);
+export const specificCarButton = document.getElementById(`specificCarButton`);
+//READ Car Name (READ)
+export const getCarByName = document.getElementById(`getCarByName`);
+export const getCarByNameButton = document.getElementById(`getCarByNameButton`);
+//Create Car (POST)
+export const name = document.getElementById(`name`);
+export const colour = document.getElementById(`colour`);
+export const make = document.getElementById(`make`);
+export const model = document.getElementById(`model`);
+export const doors = document.getElementById(`doors`);
+export const garageID = document.getElementById(`garageID`);
+export const createCarButton = document.getElementById(`createCarButton`);
+export const carOutput = document.getElementById(`carOutput`);
+//Delete Car (DELETE)
+export const deleteCarID = document.getElementById(`deleteCarID`);
+export const deleteCarButton = document.getElementById(`deleteCarButton`);
+//Update Car (PUT)
+export const updateCarName = document.getElementById(`updateCarName`);
+export const updateColour = document.getElementById(`updateColour`);
+export const updateMake = document.getElementById(`updateMake`);
+export const updateModel = document.getElementById(`updateModel`);
+export const updateDoors = document.getElementById(`updateDoors`);
+export const updateCarID = document.getElementById(`updateCarID`);
+export const updateCarButton = document.getElementById(`updateCarButton`);
