9cd86d2c2b98ce9b174ad7b6e07bc43d04513f80
Ayshwaria Patel <ashi_patel@icloud.com>
2022-02-08 10:53:32 +0000

    Update/Delete

--- cars.js
+++ cars.js
@@ -2,31 +2,113 @@
 
 import * as DOM from './dom.js';
 
-DOM.UpdateCar.onclick = () => PutCar();
-DOM.DeleteCar.onclick = () => DeleteCar();
+const writeItem = item => {
+    const child = document.createElement(`li`);
+    child.id = item._id;
+    child.innerHTML = `${JSON.stringify(item)}`;
+    DOM.CarOutput.appendChild(child);
+}
 
-//Delete (DELETE)
-const DeleteCar = () => {
+//Read (GET)
+const ReadCar = () => {
     DOM.CarOutput.innerHTML = ``;
+  
+    axios.get(`http://localhost:8080/car/read`)
+      .then((response) => {
+        if (!Array.isArray(response.data)) {
+          writeItem(response.data);
+        } else {
+          for (let item of response.data) {
+            writeItem(item);
+          }
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
+} 
 
-    axios.delete(`http://localhost:8080/car/delete/${DOM.CarId.value}`)
+//Create (POST)
+const CreateCar = () => {
+    axios.post(`http://localhost:8080/car/create`,
+        {
+          "name": `${DOM.name.value}`,
+          "colour":`${DOM.colour.value}`,
+          "make": `${DOM.make.value}`,
+          "model": `${DOM.model.value}`,
+          "doors": `${DOM.doors.value}`,
+        })
+      
         .then((response) => {
             console.log(response);
-            DOM.CarOutput.innerHTML = `${JSON.stringify(response.data)}`;
-        }) .catch((err) => {
+            getGarage();
+        }).catch((err) => {
             console.log(err);
-          });
-}
+    })};
+
+
+/*
+//Read (GET) - by ID//
+const ReadCarId = () => {
+    DOM.CarOutput.innerHTML = ``;
+  
+    axios.get(`http://localhost:8080/car/read/${DOM.ReadCarIdInput.value}`)
+      .then((response) => {
+        console.log(response);
+        DOM.CarOutput.innerHTML = `${JSON.stringify(response.data)}`;
+      }).catch((err) => {
+        console.log(err);
+      });
+  } 
+
+
+DOM.ReadCarId.onclick = () => ReadCarId();
+*/
 
 //Update (PUT)
 const PutCar = () => {
     DOM.CarOutput.innerHTML = ``;
 
-    axios.put(`http://localhost:8080/car/update/${DOM.CarId.value}`)
+    axios.put(`http://localhost:8080/car/update/${DOM.CarId.value}`, 
+        {
+        "name" : `${DOM.CarNameUpdate.value}`, 
+        "model" : `${DOM.CarModelUpdate.value}`, 
+        "make" : `${DOM.CarMakeUpdate.value}`, 
+        "colour" : `${DOM.CarColourUpdate.value}`, 
+        "doors" : `${DOM.CarDoorUpdate.value}`, 
+        "garage" : `${DOM.CarGarageUpdate.value}`
+        })
+
         .then((response) => {
             console.log(response);
-            DOM.CarOutput.innerHTML = `${JSON.stringify(response.data)}`;
+            ReadCar();
+            DOM.CarNameUpdate.value = ``;
+            DOM.CarMakeUpdate.value = ``;
+            DOM.CarModelUpdate.value = ``;
+            DOM.CarColourUpdate.value = ``;
+            DOM.CarDoorUpdate.value = ``;
+            DOM.CarGarageUpdate.value =``;
         }).catch((err) => {
             console.log(err);
         });
 }
+DOM.UpdateCar.onclick = () => PutCar();
+
+//Delete (DELETE)
+const DeleteCar = () => {
+    DOM.CarOutput.innerHTML = ``;
+
+    axios.delete(`http://localhost:8080/car/delete/${DOM.DeleteCarId.value}`)
+        .then((response) => {
+            console.log(response);
+            DOM.DeleteCarId.value = ``;
+            ReadCar();
+        }) .catch((err) => {
+            console.log(err);
+          });
+}
+DOM.DeleteCar.onclick = () => DeleteCar();
+
+DOM.ReadCar.onclick = () => ReadCar()
+//DOM.CreateCar.onclick = () => CreateCar();
+DOM.CreateCar.onclick = () => { console.log(DOM.colour.value.toString(),DOM.doors.value,DOM.make.value, DOM.model.value, DOM.name.value);
+    CreateCar();}
--- dom.js
+++ dom.js
@@ -4,9 +4,10 @@ export const CarOutput = document.getElementById(`CarOutput`);
 
 //Update (PUT) car
 export const UpdateCar = document.getElementById(`CarUpdate`);
+export const UpdateCarId = document.getElementById(`UpdateCarID`);
 
-export const CarModelUpdate = document.getElementById(`ModelUpdate`);
 export const CarNameUpdate = document.getElementById(`NameUpdate`);
+export const CarModelUpdate = document.getElementById(`ModelUpdate`);
 export const CarColourUpdate = document.getElementById(`ColourUpdate`);
 export const CarMakeUpdate = document.getElementById(`MakeUpdate`);
 export const CarDoorUpdate = document.getElementById(`DoorsUpdate`);
@@ -14,3 +15,21 @@ export const CarGarageUpdate = document.getElementById(`GarageUpdate`);
 
 //Delete (DELETE) car
 export const DeleteCar = document.getElementById(`CarDelete`);
+export const DeleteCarId = document.getElementById(`DeleteCarID`);
+
+//READ Car
+export const ReadCar = document.getElementById(`ReadCar`);
+
+//READ Car ID (READ)
+export const ReadCarIdInput = document.getElementById(`ReadCarIdInput`);
+export const ReadCarId = document.getElementById(`ReadCarId`);
+
+
+//Create Car (POST)
+export const name = document.getElementById(`name`);
+export const colour = document.getElementById(`colour`);
+export const make = document.getElementById(`make`);
+export const model = document.getElementById(`model`);
+export const doors = document.getElementById(`doors`);
+export const garageID = document.getElementById(`garageID`);
+export const CreateCar = document.getElementById(`createCarButton`);
